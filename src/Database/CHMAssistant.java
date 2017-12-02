d13ae0a2fe36348aeca0f834825f4d0e1c9aa3b3
shawn <shawnyang610@gmail.com>
2017-12-02 03:19:32 -0500

    again

---
+++ src/Database/CHMAssistant.java
@@ -0,0 +1,43 @@
+package Database;
+
+import java.util.ArrayList;
+
+public class CHMAssistant {
+	
+	public static ArrayList<ArrayList<String>> filterLists (String[] in_header, ArrayList<ArrayList <String>> in_list){
+		int stringLength = in_header[0].length();
+		ArrayList<ArrayList<String>> tempList = new ArrayList<>();
+		for (ArrayList<String> a: in_list) {
+			if (a.get(0).substring(0, stringLength-1).equals(in_header[0])) {
+				tempList.add(a);
+			}
+		}
+		return tempList;
+	}
+
+	public static String[] removeHeadersFromListThenToArray(String[] in_header, ArrayList<String> in_list){
+		int i=0;
+		String temp="";
+		String[] tempAry = new String[in_list.size()];
+		for (String s:in_list) {
+			temp=s;
+			temp.replaceAll(in_header[i]+"=", "");
+			tempAry[i]=temp;
+			i++;
+		}
+		return tempAry;
+	}
+	
+	//this method also filters out the entries without the specified headers.
+	public static String[][] listTo2DStringAry (String[] in_header, ArrayList<ArrayList <String>> in_list){
+		String[][] tempAry=new String[in_list.size()][];
+		int i=0;
+		for (ArrayList<String> a: in_list) {
+			tempAry[i]=removeHeadersFromListThenToArray(in_header, a);
+			i++;
+		}
+		return tempAry;
+	}
+	
+}
+
